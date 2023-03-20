d142e16197cdb3109b7a1481169764358b14f076
Viktor <krstevviktor996@gmail.com>
2023-03-20 23:52:29 +0100

    5th commit

--- app/src/main/java/com/example/gitgithubtest/MainActivity.kt
+++ app/src/main/java/com/example/gitgithubtest/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         println("Change 2")
 
         println("Change 1")
-        println("Hello form your friend")
+        println("heyj mamomjnse 5h commit")
 
         println("sdfdsfasd")
 
